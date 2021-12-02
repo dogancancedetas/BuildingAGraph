abc969da8f7323f63b6cd10f5508190d94d968a9
Dogancan Cedetas <dogancancedetas@hotmail.com>
2021-12-03 00:37:24 +0300

    Graph grid

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -10,6 +10,9 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
     Transform[] points;
 
     private void Awake()
@@ -17,13 +20,20 @@ public class Graph : MonoBehaviour
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+
             Transform point = points[i] = Instantiate(pointPrefab);
             
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -33,12 +43,13 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
