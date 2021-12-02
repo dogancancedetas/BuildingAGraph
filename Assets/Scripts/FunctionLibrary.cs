2d698b27574c348c2d8ce1eb65a2eb4c21afa93f
Dogancan Cedetas <dogancancedetas@hotmail.com>
2021-12-03 00:41:04 +0300

    diagonal ve triple wave tamamlandı

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -24,8 +24,9 @@ public static class FunctionLibrary
     public static float MultiWave(float x, float z, float t)
     {
        float y = Sin(PI * (x + 0.5f * t));
-        y += 0.5f * Sin(2f * PI * (x + t));
-        return y * (2f / 3f);
+        y += 0.5f * Sin(2f * PI * (z + t));
+        y += Sin(PI * (x + z + 0.25f * t));
+        return y * (1f / 2.5f);
     }
 
     public static float Ripple(float x, float z, float t)
